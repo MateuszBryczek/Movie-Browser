8f3a296fa6039a3cc43a4b2f9ee09469ee9e9aa5
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-11-27 22:19:47 +0100

    header and footer added to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { GlobalStyle } from "./common/GlobalStyle";
 import Container from "./common/Container";
+import Header from "./common/Header"
 import Tile from "./common/Tile";
+import Footer from "./common/Footer";
 
 export const App = () => {
   return (
     <>
     <GlobalStyle />
+    <Header />
       <Container>
         <Tile>
           test
         </Tile>
       </Container>
-      </>
+      <Footer />
+    </>
   );
 }
 
