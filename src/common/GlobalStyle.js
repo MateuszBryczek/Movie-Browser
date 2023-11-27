402d0b3012afad57e962ec979639c3b67ede9a7d
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-11-27 21:33:41 +0100

    font added

--- src/common/GlobalStyle.js
+++ src/common/GlobalStyle.js
@@ -10,6 +10,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: ${({ theme }) => theme.color.Grey};
+        font-family: 'Poppins', sans-serif;
+        background: ${({ theme }) => theme.color.Whisper};
     }
 `;
