f4954325d044be73401dd489ab2405af63441b88
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-11-27 21:11:56 +0100

    theme.js added

---
+++ src/common/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+    color: {
+        White: "#FFFFFF",       //white
+        Whisper: "#F5F5FA",     //light grey
+        Mystic: "#E4E6F0",      //grey
+        Waterloo: "#7E839A",    //dark grey
+        MineShaft: "#333333",   //darker grey
+        Woodsmoke: "#18181B",   //black
+        Candlelight: "#FCD420", //yellow
+        ScienceBlue: "#0044CC", //blue
+        PattensBlue: "#D6E4FF", //light blue
+    }
+};
