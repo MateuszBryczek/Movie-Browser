7aed94de9534e31ed8a6015f03d7a0888046a32c
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-11-28 14:24:11 +0100

    added Globalthemes to components in theme.js

--- src/common/theme.js
+++ src/common/theme.js
@@ -1,5 +1,6 @@
-export const theme = {
-    color: {
+import { ThemeProvider } from 'styled-components';
+
+const colorNames = {
         White: "#FFFFFF",       //white
         Whisper: "#F5F5FA",     //light grey
         Mystic: "#E4E6F0",      //grey
@@ -9,5 +10,45 @@ export const theme = {
         Candlelight: "#FCD420", //yellow
         ScienceBlue: "#0044CC", //blue
         PattensBlue: "#D6E4FF", //light blue
-    }
 };
+
+export const GlobalTheme ={
+    colors: {
+        background: colorNames.Whisper,
+        primary: colorNames.White,
+        textPrimary: colorNames.Woodsmoke,
+        header: {
+            textPrimary: colorNames.White,
+            primary: colorNames.Woodsmoke,
+            secondary: colorNames.White,
+        },
+        searchBar: {
+            textPrimary: colorNames.Waterloo,
+            primary: colorNames.White,
+            secondary: colorNames.Waterloo,
+        },
+        tile: {
+            textPrimary: colorNames.Woodsmoke,
+            textSecondary: colorNames.Waterloo,
+            primary: colorNames.White,
+            secondary: colorNames.Mystic,
+            star: colorNames.Candlelight,
+        },
+        footer: {
+            textPrimary: colorNames.Woodsmoke,
+            textSecondary: colorNames.Waterloo,
+        },
+        footerButton: {
+            textPrimary: colorNames.Woodsmoke,
+            textSecondary: colorNames.MineShaft,
+            primary: colorNames.Waterloo,
+            secondary: colorNames.PattensBlue,
+            iconPrimary: colorNames.MineShaft,
+            iconSecondary: colorNames.ScienceBlue,
+        },
+    },
+};
+
+export default function Theme({ children }) {
+    return <ThemeProvider theme={GlobalTheme}>{children}</ThemeProvider>;
+}
