7bc768815d3adc1063029b38afbde4d9a695ddef
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-01 15:25:47 +0100

    deleted Tile and Footer from App.js and added movieList

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import { GlobalStyle } from "./common/GlobalStyle";
 import Theme from "./common/theme";
 import Container from "./common/Container";
 import Header from "./common/Header";
-import Tile from "./common/Tile";
-import Footer from "./common/Footer";
 import Navigation from "./Navigation";
 import Pagination from "./common/Pagination";
+import MovieList from "./MovieList";
 
 export const App = () => {
   return (
