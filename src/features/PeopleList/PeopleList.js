ad2b28037e16a39bc324b911ea413114027bb616
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-03 17:35:28 +0100

    created PeopleList.js

---
+++ src/features/PeopleList/PeopleList.js
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+const PeopleList () => {
+    const people = useSelector(selectPeople);
+    const error = useSelector(selectError);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPeople());
+      }, [dispatch]);
+
+      return (
+        
+      );
+};
+
+export default PeopleList;
