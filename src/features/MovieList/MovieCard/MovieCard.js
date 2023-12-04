5403fab9a62db8ea9e7966603d871bbf75e0751c
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-04 14:01:05 +0100

    styled movieCard,fixed rating value,

--- src/features/MovieList/MovieCard/MovieCard.js
+++ src/features/MovieList/MovieCard/MovieCard.js
@@ -1,17 +1,23 @@
-import { Poster, Title, Date, Tags, Rating, Votes, Description, } from "./styled";
+import { Poster, Title, Date, Tags, Rating, Votes, Description,StyledCard,RateWrapper } from "./styled";
 import { IMG_URL_500 } from "../../config";
+import {ReactComponent as Star} from "../../../images/starVector.svg";
 
 const MovieCard = ({ original_title, release_date, genres, vote_average, vote_count, poster_path }) => {
     return (
         <>
+        <StyledCard>
             <Poster src={IMG_URL_500+poster_path} ></Poster>
             <Description>
                 <Title>{original_title}</Title>
                 <Date>{release_date}</Date>
             </Description>
             <Tags>{genres}</Tags>
-            <Rating>{vote_average}</Rating>
+            <RateWrapper>
+                <Star/>
+            <Rating>{vote_average.toFixed(1)}</Rating>
             <Votes>{vote_count} votes</Votes>
+            </RateWrapper>
+            </StyledCard>
         </>
     );
 };
