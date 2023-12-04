63d79299e57a6a6c17fb5019cb62e226506c4c1f
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-04 15:11:39 +0100

    styled tags and ratings

--- src/features/MovieList/MovieCard/styled.js
+++ src/features/MovieList/MovieCard/styled.js
@@ -15,47 +15,60 @@ export const Poster = styled.img`
 `;
 
 export const Description = styled.div`
-  display: inline-flex;
+  display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: 8px;
 `;
 
-export const Title = styled.h1`
+export const Title = styled.p`
   color: ${({ theme }) => theme.colors.tile.textPrimary};
   font-size: 22px;
   font-weight: 900;
-  line-height: 130%;
+  line-height: 22px;
+  margin: 15px 0px 5px;
 `;
 
-export const Date = styled.h2`
+export const Date = styled.p`
   color: ${({ theme }) => theme.colors.tile.textSecondary};
   font-size: 16px;
-  font-weight: 400;
-  line-height: 150%;
+  font-weight: 500;
+  line-height: 16px;
+  margin: 0px;
 `;
 
 export const Tags = styled.div`
+  width: 77px;
+  height: 36px;
   display: flex;
   padding: 8px 16px;
   align-items: center;
   gap: 10px;
+  color: ${({ theme }) => theme.colors.tile.textPrimary};
+  background-color: ${({ theme }) => theme.colors.tile.secondary};
+  margin: 0px 0px 20px;
+`;
+
+export const TagsWrapper = styled.div`
+  padding: 10px 0px 20px;
 `;
 
 export const Rating = styled.a`
   font-size: 16px;
-  font-weight: 600;
+  font-weight: 900;
   line-height: 150%;
+  color: ${({ theme }) => theme.colors.tile.textPrimary};
 `;
 
 export const Votes = styled.a`
   font-size: 16px;
-  font-weight: 400;
+  font-weight: 500;
   line-height: 150%;
-  padding-left: 10px;
+  color: ${({ theme }) => theme.colors.tile.textSecondary};
 `;
 
 export const RateWrapper = styled.div`
-display:flex;
-flex-direction: row;
-`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+`;
