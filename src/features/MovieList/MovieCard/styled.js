e7a683cdb389fa419e552d57f822767cf4244270
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-04 14:02:26 +0100

    added start, added wrapping for items

--- src/features/MovieList/MovieCard/styled.js
+++ src/features/MovieList/MovieCard/styled.js
@@ -1,48 +1,61 @@
 import styled from "styled-components";
 
+export const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 15px;
+`;
+
 export const Poster = styled.img`
-    width: 292px;
-    height: 434px;
-    flex-shrink: 0;
-    border-radius: 5px;
+  width: 292px;
+  height: 434px;
+  flex-shrink: 0;
+  border-radius: 5px;
 `;
 
 export const Description = styled.div`
-    display: inline-flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 8px;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
 `;
 
 export const Title = styled.h1`
-    color: ${({ theme }) => theme.colors.tile.textPrimary};
-    font-size: 22px;
-    font-weight: 500;
-    line-height: 130%;
+  color: ${({ theme }) => theme.colors.tile.textPrimary};
+  font-size: 22px;
+  font-weight: 900;
+  line-height: 130%;
 `;
 
 export const Date = styled.h2`
-    color: ${({ theme }) => theme.colors.tile.textSecondary};
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 150%;
+  color: ${({ theme }) => theme.colors.tile.textSecondary};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
 `;
 
 export const Tags = styled.div`
-    display: flex;
-    padding: 8px 16px;
-    align-items: center;
-    gap: 10px;
+  display: flex;
+  padding: 8px 16px;
+  align-items: center;
+  gap: 10px;
 `;
 
 export const Rating = styled.a`
-    font-size: 16px;
-    font-weight: 600;
-    line-height: 150%;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
 `;
 
 export const Votes = styled.a`
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 150%;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
+  padding-left: 10px;
 `;
+
+export const RateWrapper = styled.div`
+display:flex;
+flex-direction: row;
+`
