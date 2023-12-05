da7a8a5618b6ad39e301b1a59e70617ceb245123
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-05 19:31:37 +0100

    added GenresList to MovieCard

--- src/features/MovieList/MovieCard/MovieCard.js
+++ src/features/MovieList/MovieCard/MovieCard.js
@@ -1,7 +1,9 @@
 import { Poster, Title, Date, Tags, Rating, Votes, Description, } from "./styled";
 import { IMG_URL_500 } from "../../config";
+import GenreList from "../Genras/GenresList";
 
-const MovieCard = ({ original_title, release_date, genres, vote_average, vote_count, poster_path }) => {
+const MovieCard = ({ original_title, release_date, genre_ids, vote_average, vote_count, poster_path }) => {
+
     return (
         <>
             <Poster src={IMG_URL_500+poster_path} ></Poster>
@@ -9,7 +11,7 @@ const MovieCard = ({ original_title, release_date, genres, vote_average, vote_co
                 <Title>{original_title}</Title>
                 <Date>{release_date}</Date>
             </Description>
-            <Tags>{genres}</Tags>
+            <Tags><GenreList genreIds={genre_ids} /></Tags>
             <Rating>{vote_average}</Rating>
             <Votes>{vote_count} votes</Votes>
         </>
