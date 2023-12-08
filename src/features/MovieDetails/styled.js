e1483c935c21756c34f70fa514258fb4dbf9fc9d
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-08 15:26:04 +0100

    started with MovieDetails

---
+++ src/features/MovieDetails/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PosterTile = styled.div``;
+
+export const Poster = styled.img``;
+
+export const Title = styled.img``;
+
+export const Rating = styled.img``;
+
+export const Votes = styled.img``;
