ff0f45e429fbc6e9f4635593ff570c21d25e2729
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-08 15:24:59 +0100

    centered TilesWrapper for MovieList and PeopleList. added hover effect for Tiles

--- src/features/PeopleList/styled.js
+++ src/features/PeopleList/styled.js
@@ -4,7 +4,7 @@ export const TilesWrapper = styled.div`
   max-width: 1368px;
   height: 1391px;
   margin-top: 30px;
-  margin: 50px 50px 50px 50px;
+  margin: 50px auto;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr;
@@ -23,4 +23,18 @@ export const TilesWrapper = styled.div`
 export const PeopleTile = styled.div`
   max-width: 208px;
   max-height: 339px;
+  border-radius: 5px;
+  gap: 12px;
+  padding: 16px;
+  box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
+  background-color: ${({ theme }) => theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &:hover {
+    transform: scale(105%);
+    transition: 0.3s;
+    cursor: pointer;
+  }
 `;
