9c07384dbe3f8b77646d0ba63cb64b9a64daaa2f
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-15 16:22:59 +0100

    added Moviedetails to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,7 +1,11 @@
 import { all } from "redux-saga/effects";
-import { watchFetchMovies } from "./features/MovieList/movieSaga";
+import { watchFetchMovies, watchfetchMovieDetails } from "./features/MovieList/movieSaga";
 import { watchFetchPeople } from "./features/PeopleList/peopleSaga";
 
 export default function* rootSaga() {
-  yield all([watchFetchMovies(), watchFetchPeople()]);
+  yield all([
+    watchFetchMovies(), 
+    watchFetchPeople(),
+    watchfetchMovieDetails(),
+  ]);
 }
