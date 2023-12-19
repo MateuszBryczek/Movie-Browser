efac9b309491a34091ebf371306563c5dacba066
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-19 17:36:16 +0100

    added page to url

--- src/features/config.js
+++ src/features/config.js
@@ -4,7 +4,7 @@ export const apiKey= "5436c11ec3f3706d8f349dbc68c7265e"
 export const POSTER_URL_SMALL = "https://image.tmdb.org/t/p/w500"
 export const IMG_URL_SMALL = "https://image.tmdb.org/t/p/w300"
 export const basicUrl = "https://api.themoviedb.org/3/";
-export const popularMoviesURL = `${basicUrl}movie/popular?api_key=${apiKey}`
+export const popularMoviesURL = `${basicUrl}movie/popular?api_key=${apiKey}&page=`
 
 export default axios.create({
     baseURL: "https://api.themoviedb.org/3/",
