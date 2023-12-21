9c5cbb2dd502c31828c7a289b28c514d6498f0a3
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-21 13:07:07 +0100

    styled for peoplecard updated

--- src/features/PeopleList/PeopleCard/styled.js
+++ src/features/PeopleList/PeopleCard/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
 
 export const Image = styled.img`
   height: 231px;
@@ -12,14 +13,31 @@ export const Image = styled.img`
 `;
 
 export const Name = styled.h1`
-  font-weight: 500;
-  line-height: 28.6px;
-  font-size: 22px;
-  text-align: center;
-  margin: 0;
+    color: ${({ theme }) => theme.colors.tile.textPrimary};
+    max-width: 176px;
+    font-weight: 500;
+    line-height: 130%;
+    font-size: 22px;
+    text-align: center;
+    margin: 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.smallDevices}px) {
     line-height: 18.2px;
     font-size: 14px;
   }
+`;
+
+export const Description = styled.div`
+    display: flex;
+    max-height: 64px;
+    align-items: flex-start;
+    justify-content: center;
+`;
+
+export const Role = styled.a`
+    color: ${({ theme }) => theme.colors.tile.textSecondary};
+`;
+
+export const PersonDetailsLink = styled(NavLink)`
+    text-decoration: none;
 `;
