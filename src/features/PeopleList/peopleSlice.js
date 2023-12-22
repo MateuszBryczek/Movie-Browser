c04d712a4d5b03609feba19e518357a2794acb28
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-22 12:59:00 +0100

    added page to peopleslice

--- src/features/PeopleList/peopleSlice.js
+++ src/features/PeopleList/peopleSlice.js
@@ -7,6 +7,8 @@ const peopleSlice = createSlice({
     error: false,
     searchPeopleValue: "",
     isLoading: false,
+    peoplePage: 1,
+    maxPages: 500,
   },
   reducers: {
     fetchPeople: (state) => {
@@ -26,14 +28,38 @@ const peopleSlice = createSlice({
       state.isLoading = true;
       state.searchPeopleValue = searchPeopleValue;
     },
+    increment: (state) => {
+      state.peoplePage = state.peoplePage + 1;
+    },
+    decrement: (state) => {
+      state.peoplePage = state.peoplePage - 1;
+    },
+    goToLastPage: (state) => {
+      state.peoplePage = state.maxPages;
+    },
+    goToFirstPage: (state) => {
+      state.peoplePage = 1;
+    },
   },
 });
 
-export const { fetchPeople, fetchPeopleSucces, fetchPeopleError, changeSearchPeopleValue } =
-  peopleSlice.actions;
-const selectPeopleState = state => state.people;
-export const selectPeople = state => selectPeopleState(state).people;
-export const selectPeopleError = state => selectPeopleState(state).error;
-export const selectSearchPeopleValue = state => selectPeopleState(state).searchPeopleValue;
-export const selectPeopleIsLoading = state => selectPeopleState(state).isLoading;
+export const {
+  fetchPeople,
+  fetchPeopleSucces,
+  fetchPeopleError,
+  changeSearchPeopleValue,
+  increment,
+  decrement,
+  goToLastPage,
+  goToFirstPage,
+} = peopleSlice.actions;
+const selectPeopleState = (state) => state.people;
+export const selectPeople = (state) => selectPeopleState(state).people;
+export const selectPeopleError = (state) => selectPeopleState(state).error;
+export const selectSearchPeopleValue = (state) =>
+  selectPeopleState(state).searchPeopleValue;
+export const selectPeopleIsLoading = (state) =>
+  selectPeopleState(state).isLoading;
+  export const selectPeoplePage = (state) => selectPeopleState(state).peoplePage;
+
 export default peopleSlice.reducer;
