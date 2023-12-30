b70bf71479274757cd94b9a6ce0b22610f9fc949
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2023-12-30 16:23:15 +0100

    Star and TileWrapper added

--- src/features/MovieDetails/MovieDetailsCard/MovieDetailsCard.js
+++ src/features/MovieDetails/MovieDetailsCard/MovieDetailsCard.js
@@ -10,30 +10,33 @@ import {
   TagsWrapper,
   Title,
   Votes,
+  Star,
+  TileWrapper,
 } from "./styled";
-import { ReactComponent as Star } from "../../../images/starVector.svg";
 import { IMG_URL_SMALL } from "../../config";
 import GenreList from "../../MovieList/Genras/GenresList";
 
 const MovieDetailsCard = ({
   poster_path,
   title,
-  relase_date,
+  release_date,
   production_countries,
   genre_ids,
   vote_average,
   vote_count,
   overview,
 }) => {
-  return (
+  
+  return ( 
     <>
       <MovieTile>
         <Poster src={IMG_URL_SMALL + poster_path}></Poster>
+        <TileWrapper>
         <Title>{title}</Title>
-        <Date>{relase_date}</Date>
+        <Date>{release_date}</Date>
         <Section>
           Production:<Production>{}</Production>
-          Relase date:<Relase>{relase_date}</Relase>
+          Relase date:<Relase>{release_date}</Relase>
         </Section>
         <TagsWrapper>
           <GenreList genreIds={genre_ids} />
@@ -42,6 +45,7 @@ const MovieDetailsCard = ({
         <Rating>{vote_average?.toFixed(1)}</Rating>/10
         <Votes>{vote_count}</Votes>
         <Descryption>{overview}</Descryption>
+        </TileWrapper>
       </MovieTile>
     </>
   );
