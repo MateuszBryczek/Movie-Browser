e227eee7a1892d348f551b10e3e939d10c520522
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-12-31 19:18:23 +0100

    updated movieDetailsCard

--- src/features/MovieDetails/MovieDetailsCard/MovieDetailsCard.js
+++ src/features/MovieDetails/MovieDetailsCard/MovieDetailsCard.js
@@ -15,6 +15,7 @@ import {
 } from "./styled";
 import { IMG_URL_SMALL } from "../../config";
 import GenreList from "../../MovieList/Genras/GenresList";
+import noMovieImage from "../../../images/noMovieImage.svg";
 
 const MovieDetailsCard = ({
   poster_path,
@@ -26,27 +27,33 @@ const MovieDetailsCard = ({
   vote_count,
   overview,
 }) => {
+  const countryNames = production_countries
+    ?.map(country => country.name)
+    .join(", ");
 
-  const countryNames = production_countries.map(country => country.name).join(", ");
-  
-  return ( 
+  return (
     <>
       <MovieTile>
-        <Poster src={IMG_URL_SMALL + poster_path}></Poster>
+        <Poster
+          src={poster_path ? IMG_URL_SMALL + poster_path : noMovieImage}
+        ></Poster>
         <TileWrapper>
-        <Title>{title}</Title>
-        <Date>{release_date}</Date>
-        <Section>
-          Production:<Production>{countryNames}</Production>
-          Relase date:<Relase>{release_date}</Relase>
-        </Section>
-        <TagsWrapper>
-          <GenreList genreIds={genre_ids} />
-        </TagsWrapper>
-        <Star />
-        <Rating>{vote_average?.toFixed(1)}</Rating>/10
-        <Votes>{vote_count}</Votes>
-        <Descryption>{overview}</Descryption>
+          <Title>{title}</Title>
+          <Date>{release_date ? release_date : ""}</Date>
+          <Section>
+            Production:
+            <Production>{countryNames ? countryNames : "-"}</Production>
+            Relase date:<Relase>{release_date ? release_date : "-"}</Relase>
+          </Section>
+          <TagsWrapper>
+            <GenreList
+              genreNames={genre_ids}
+            />
+          </TagsWrapper>
+          <Star />
+          <Rating>{vote_average?.toFixed(1)}</Rating>/10
+          <Votes>{vote_count}</Votes>
+          <Descryption>{overview}</Descryption>
         </TileWrapper>
       </MovieTile>
     </>
