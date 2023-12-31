42947469c29874f872bd39c1d222ab7a70836fc7
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-31 14:34:35 +0100

    styled card and changed order according to figma source

--- src/features/MovieDetails/MovieDetailsCard/MovieDetailsCard.js
+++ src/features/MovieDetails/MovieDetailsCard/MovieDetailsCard.js
@@ -10,6 +10,9 @@ import {
   TagsWrapper,
   Title,
   Votes,
+  PosterWrapper,
+  TextWrapper,
+  RatingsWrapper
 } from "./styled";
 import { ReactComponent as Star } from "../../../images/starVector.svg";
 import { IMG_URL_SMALL } from "../../config";
@@ -28,20 +31,26 @@ const MovieDetailsCard = ({
   return (
     <>
       <MovieTile>
-        <Poster src={IMG_URL_SMALL + poster_path}></Poster>
-        <Title>{title}</Title>
-        <Date>{relase_date}</Date>
-        <Section>
-          Production:<Production>{}</Production>
-          Relase date:<Relase>{relase_date}</Relase>
-        </Section>
-        <TagsWrapper>
-          <GenreList genreIds={genre_ids} />
-        </TagsWrapper>
-        <Star />
-        <Rating>{vote_average?.toFixed(1)}</Rating>/10
-        <Votes>{vote_count}</Votes>
-        <Descryption>{overview}</Descryption>
+        <PosterWrapper>
+          <Poster src={IMG_URL_SMALL + poster_path}></Poster>
+        </PosterWrapper>
+        <TextWrapper>
+          <Title>{title}</Title>
+          <Date>{relase_date}</Date>
+          <Section>
+            Relase date:<Relase>{relase_date}</Relase>
+            Production:<Production>{}</Production>
+          </Section>
+          <TagsWrapper>
+            <GenreList genreIds={genre_ids} />
+          </TagsWrapper>
+          <RatingsWrapper>
+          <Star />
+          <Rating>{vote_average?.toFixed(1)}</Rating>/10
+          <Votes>{vote_count}</Votes>votes
+          </RatingsWrapper>
+          <Descryption>{overview}</Descryption>
+        </TextWrapper>
       </MovieTile>
     </>
   );
