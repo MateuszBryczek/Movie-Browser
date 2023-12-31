eb058616f37f2eac07783ea96e9da43500907c66
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-12-31 14:35:03 +0100

    styled card

--- src/features/MovieDetails/MovieDetailsCard/styled.js
+++ src/features/MovieDetails/MovieDetailsCard/styled.js
@@ -5,24 +5,37 @@ export const MovieTile = styled.div`
     color: ${({ theme }) => theme.colors.tile.textPrimary};
     box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.50);
     display: flex;
+    flex-direction: row;
     padding: 40px;
     align-items: flex-start;
     gap: 40px;
 `;
+
+export const PosterWrapper = styled.div `
+`
+export const TextWrapper = styled.div `
+`
 
 export const Poster = styled.img`
 
 `;
 
 export const Title = styled.p`
-
+font-size: 36px;
+font-weight:600;
+line-height: 43.2px;
 `;
 
 export const Date = styled.p`
+font-size: 22px;
+line-height: 26.4px;
+font-weight:400;
 
 `;
 
 export const Section = styled.div`
+display:grid;
+grid-gap:11px;
 
 `;
 
@@ -35,11 +48,23 @@ export const Relase = styled.p`
 `;
 
 export const TagsWrapper = styled.div`
+display:grid;
+grid-gap:16px;
 
 `;
 
-export const Rating = styled.a`
+export const RatingsWrapper = styled.div`
+display:grid;
+grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+height: 29px;
+width:185px;
+align-items: center;
+`
 
+export const Rating = styled.a`
+font-size: 22px;
+font-weight: 500;
+line-height: 28.6px;
 `;
 
 export const Votes = styled.a`
@@ -47,5 +72,8 @@ export const Votes = styled.a`
 `;
 
 export const Descryption = styled.div`
-
+margin-top: 20px;
+font-weight: 400;
+font-size: 20px;
+line-height: 32px;
 `;
