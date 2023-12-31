b1b7f8cbbc7cf862a6fb25fdd0f2f56ab054f1e5
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-12-31 19:23:49 +0100

    updated PersonDetailsCard

--- src/features/PersonDetails/PersonDetailsCard/PersonDetailsCard.js
+++ src/features/PersonDetails/PersonDetailsCard/PersonDetailsCard.js
@@ -1,39 +1,47 @@
 import { IMG_URL_SMALL } from "../../config";
+import noPersonImage from "../../../images/noPersonImage.svg";
 import {
-    PersonTile,
-    Image,
-    Name,
-    Section,
-    Date,
-    BirthPlace,
-    Description,
-    TileWrapper,
-    InfoSecion,
+  PersonTile,
+  Image,
+  Name,
+  Section,
+  Date,
+  BirthPlace,
+  Description,
+  TileWrapper,
+  InfoSecion,
+} from "./styled";
 
-} from "./styled"
-
-
-const PersonDetailsCard = ({ profile_path, name, birthday, place_of_birth, biography }) => {
-
-    return (
-        <>
-            <PersonTile>
-            <TileWrapper>
-            <Image src={IMG_URL_SMALL + profile_path}/>
-            <Section>
+const PersonDetailsCard = ({
+  profile_path,
+  name,
+  birthday,
+  place_of_birth,
+  biography,
+}) => {
+  return (
+    <>
+      <PersonTile>
+        <TileWrapper>
+          <Image
+            src={profile_path ? IMG_URL_SMALL + profile_path : noPersonImage}
+          />
+          <Section>
             <Name>{name}</Name>
             <InfoSecion>
-                Date of birth: <Date>{birthday}</Date>
+              Date of birth:
+              <Date>{birthday ? birthday : "-"}</Date>
             </InfoSecion>
             <InfoSecion>
-                Place of birth: <BirthPlace>{place_of_birth}</BirthPlace>
+              Place of birth:
+              <BirthPlace>{place_of_birth ? place_of_birth : "-"}</BirthPlace>
             </InfoSecion>
             <Description>{biography}</Description>
-            </Section>
-            </TileWrapper>
-            </PersonTile>
-        </>
-    );
+          </Section>
+        </TileWrapper>
+      </PersonTile>
+    </>
+  );
 };
 
 export default PersonDetailsCard;
