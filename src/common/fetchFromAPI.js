33286cd7c9074ac224ec3964360f34709aca94d3
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-01-04 15:24:12 +0100

    in progress

---
+++ src/common/fetchFromAPI.js
@@ -0,0 +1,24 @@
+// import { buildQueryString } from "./buildQueryString";
+// import axios from 'axios';
+
+
+//  const apiKey= "5436c11ec3f3706d8f349dbc68c7265e";
+//  const basicUrl = "https://api.themoviedb.org/3/";
+
+
+// export const fetchFromAPI = async ({path, parameters})=>{
+// const defaultParameters = {
+//     api_key: apiKey,
+// };
+// const allParameters = {
+//     ...defaultParameters,
+//     ...apiKey(parameters || {}),
+// };
+// const response = await axios.get
+// (`${basicUrl}${path}?${buildQueryString(allParameters)}`);
+
+
+
+// }
+
+
