0a8631d97dc55159c1c8acbb2e3a152302c5c3fc
Paweł Brejnak <pawelbrejnak6@gmail.com>
2024-01-04 22:46:41 +0100

    added searching and changed palceholder for detailsPages

--- src/common/Navigation/SearchBar/index.js
+++ src/common/Navigation/SearchBar/index.js
@@ -1,14 +1,15 @@
 import { Search, SearchWrapper, SearchImg } from "./styled";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  useHistory,
+  useLocation,
+} from "react-router-dom/cjs/react-router-dom.min";
 import search from "./../../../images/search.svg";
 import {
   useReplaceQueryParameter,
   useQueryParameter,
 } from "../../../features/queryParameter";
 import { changeSearchPeopleValue } from "../../../features/Slices/peopleSlice";
-import {
-  changeSearchMoviesValue,
-} from "../../../features/Slices/movieSlice";
+import { changeSearchMoviesValue } from "../../../features/Slices/movieSlice";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import searchQueryParamName from "../../../features/searchQueryParamName";
@@ -18,15 +19,24 @@ const SearchBar = () => {
   const query = useQueryParameter(searchQueryParamName);
   const dispatch = useDispatch();
   const location = useLocation();
+  const history = useHistory();
 
   useEffect(() => {
     location.pathname === "/peoplelist"
       ? dispatch(changeSearchPeopleValue(query))
-      : dispatch(changeSearchMoviesValue(query))
+      : dispatch(changeSearchMoviesValue(query));
   }, [dispatch, location, query]);
 
   const oninputChange = ({ target }) => {
     replaceQueryParameter({ key: searchQueryParamName, value: target });
+    if (
+      location.pathname !== "/peoplelist" &&
+      location.pathname !== "/movieList"
+    ) {
+      location.pathname.slice(0, 14) === "/personDetails"
+        ? history.push(`/peoplelist?query=${target.value}`)
+        : history.push(`/movieList?query=${target.value}`);
+    }
   };
 
   return (
@@ -34,13 +44,13 @@ const SearchBar = () => {
       <SearchImg src={search} alt="" />
       <Search
         placeholder={
-          location.pathname === "/peoplelist"
+          location.pathname === "/peoplelist" ||
+          location.pathname.slice(0, 14) === "/personDetails"
             ? "Search for people..."
             : "Search for movies..."
         }
         value={query || ""}
         onChange={oninputChange}
-        disabled={location.pathname !== "/peoplelist" && location.pathname !== "/movielist" ? true : false}
       />
     </SearchWrapper>
   );
