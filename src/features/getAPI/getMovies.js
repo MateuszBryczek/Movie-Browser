c92419bbfe1ac315a66f05fc2fb01d16ad729d05
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-01-05 19:04:29 +0100

    rewrite getMovies in order to use fetchBothTypesApi

--- src/features/getAPI/getMovies.js
+++ src/features/getAPI/getMovies.js
@@ -1,12 +1,24 @@
+import {fetchFromAPI} from "../getAPI/bothApiTypes";
 import config from "./config";
 
-export const getMovies = async moviePage => {
-  const { data } = await config.get(`movie/popular?page=${moviePage}`);
-  if (!data.ok) {
-    new Error(data.statusText);
-  }
-  return await { ...data };
-};
+
+//funkcja getMovies zaciaga przybierajac parametr moviePage,
+//do jej
+//https://api.themoviedb.org/3/movie/popular?page=1&api_key=5436c11ec3f3706d8f349dbc68c7265e
+
+
+// export const getMovies = async (moviePage) => {
+//   const { data } = await config.get(`movie/popular?page=${moviePage}`);
+//   //tutaj musze podmienic w get, zeby bylo to budowanie przy pomocy buildQuery
+
+//   if (!data.ok) {
+//     new Error(data.statusText);
+//   }
+//   return await { ...data };
+// };
+
+export const getMovies = fetchFromAPI({path:"movie/popular"})
+//nie dokleja page 
 
 //https://api.themoviedb.org/3/search/movie?query=troll&page=1&api_key=5436c11ec3f3706d8f349dbc68c7265e
 export const getSearchedMovies = async (searchValue, moviePage) => {
@@ -21,7 +33,7 @@ export const getSearchedMovies = async (searchValue, moviePage) => {
   return await { ...data };
 };
 
-export const getMoviesDetails = async movieId => {
+export const getMoviesDetails = async (movieId) => {
   try {
     const { data } = await config.get(`movie/${movieId}`);
     return { ...data };
@@ -30,7 +42,7 @@ export const getMoviesDetails = async movieId => {
   }
 };
 
-export const getPeopleForMovie = async movieId => {
+export const getPeopleForMovie = async (movieId) => {
   try {
     const { data } = await config.get(`movie/${movieId}/credits`);
     return { ...data };
