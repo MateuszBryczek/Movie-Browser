d2398305164af548130327319d013d6dad17216f
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-01-05 19:03:53 +0100

    added universal fetch template for all types of api

--- src/features/getAPI/bothApiTypes.js
+++ src/features/getAPI/bothApiTypes.js
@@ -1,28 +1,24 @@
-import {buildQueryString} from "../../common/buildQueryString";
-const apiKey = "5436c11ec3f3706d8f349dbc68c7265e"
-export const basicUrl = "https://api.themoviedb.org/3/";
 import axios from "axios";
 
+import { buildQueryString } from "../../common/buildQueryString";
+const apiKey = "5436c11ec3f3706d8f349dbc68c7265e";
+const basicUrl = "https://api.themoviedb.org/3/";
 
-export const fetchFromAPI = async ({path, parameters})=>{
-const defaultParameters = {
-api_key: apiKey,
-};
+export const fetchFromAPI = async ({ path, parameters }) => {
+  const defaultParameters = {
+    api_key: apiKey,
+  };
 
-const allParameters = {
+  const allParameters = {
     ...defaultParameters,
-    ...apiKey(parameters || {}),
-};
-const {data}=
-await axios.get(`${basicUrl}${path}?${buildQueryString(allParameters)}`);
+    ...(parameters || {}),
+  };
+  const { data } = await axios.get(
+    `${basicUrl}${path}?${buildQueryString(allParameters)}`
+  );
 
-if (!Response.ok)
-{
+  if (!Response.ok) {
     new Error(data.statusText);
   }
   return await { ...data };
-
 };
-
-
-
