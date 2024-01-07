ce0fe10c570f9c63cc1d8930e5a3e90f683df64e
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-01-07 14:02:49 +0100

    started listsaga

--- src/common/ListPage/listSaga.js
+++ src/common/ListPage/listSaga.js
@@ -1,31 +1,31 @@
-import {call, put, takeLatest, delay} from 'redux-saga/effects';
+// import {call, put, takeLatest, delay} from 'redux-saga/effects';
 
-export function* listSaga({
-    actions,
-    search, 
-    getPopular,
-}) {
-function fetchListHandler ({payload:{query,page}}){
-    yield delay (500);
+// export function* listSaga({
+//     actions,
+//     search, 
+//     getPopular,
+// }) {
+// function fetchListHandler ({payload:{query,page}}){
+//     yield delay (500);
 
-    try {
-const response = yield (
-    query?.trim()
-    ?call(search,{page,query})
-    :call(getPopular,{page})
-);
+//     try {
+// const response = yield (
+//     query?.trim()
+//     ?call(search,{page,query})
+//     :call(getPopular,{page})
+// );
 
-yield put (actions.fetchSuccess({
-data: response.results.data,
-pagination: {
-    page:response.results.data.page,
-    totalPages: response.results.data.total_pages,
-    totalResults: response.results.data.total_results,
-},
-}));
-}catch (error){
-yield put(actions.fetchError());
-}
-}
-yield takeLatest(actions.fetch.type, fetchListHandler);
-}
+// yield put (actions.fetchSuccess({
+// data: response.results.data,
+// pagination: {
+//     page:response.results.data.page,
+//     totalPages: response.results.data.total_pages,
+//     totalResults: response.results.data.total_results,
+// },
+// }));
+// }catch (error){
+// yield put(actions.fetchError());
+// }
+// }
+// yield takeLatest(actions.fetch.type, fetchListHandler);
+// }
